dfb775aa7d39c23c8d60be4875996ad6f1466d8d
Stuart Fleisher <stufleisher@gmail.com>
2024-02-04 12:47:13 -0500

    removing dist

--- dist/client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/**We use common js for other imports to avoid a transpiling issue related to
- * extensions and paths differing in testing and dev environments
- */
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-module.exports = prisma;
